bebf2b96b531345608bea39c6759224c18196cc6
Денисова Татьяна <124772661+DenisowaTanya@users.noreply.github.com>
2023-05-19 17:02:19 +0300

    Update CorrectQuery.sql

--- LESSON2/CorrectQuery.sql
+++ LESSON2/CorrectQuery.sql
@@ -1,19 +1,4 @@
-/*Заказы (Orders) с ценой товара (UnitPrice) более 100$ 
-либо количеством единиц (Quantity) товара более 20 штук
-и присутствующей датой комплектации всего заказа (PickingCompletedWhen).
-Вывести:
-* OrderID
-* дату заказа (OrderDate) в формате ДД.ММ.ГГГГ
-* название месяца, в котором был сделан заказ
-* номер квартала, в котором был сделан заказ
-* треть года, к которой относится дата заказа (каждая треть по 4 месяца)
-* имя заказчика (Customer)
-Добавьте вариант этого запроса с постраничной выборкой,
-пропустив первую 1000 и отобразив следующие 100 записей.
-
-Сортировка должна быть по номеру квартала, трети года, дате заказа (везде по возрастанию).
-
-Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.*/
+--РџРµСЂРµРїРёСЃР°Р»Р° 1 Р·Р°РїСЂРѕСЃ С‡РµСЂРµР· РѕРїРµСЂР°С‚РѕСЂ CASE
 
 USE WideWorldImporters
 
@@ -23,7 +8,7 @@ DATENAME(month,OrderDate) as [MonthName],
 DATEPART(quarter,OrderDate) as [Quarter],
 CASE WHEN month(OrderDate)<=4 THEN 1
 	WHEN month(OrderDate) between 5 and 8 THEN 2
-	WHEN month(OrderDate)>=8 THEN 3
+	WHEN month(OrderDate)>=9 THEN 3
 END as [Part],
 t5.CustomerName
 FROM [Sales].[OrderLines] t2
